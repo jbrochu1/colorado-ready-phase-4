8b55a3a907bacb021ae37f6078b815133f5f9dc8
jbrochu1 <111677770+jbrochu1@users.noreply.github.com>
2022-12-06 10:41:08 -0700

    tailwind react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,33 @@
-import { useState, useEffect } from 'react'
-import LogIn from './components/LogIn'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [places, setPlaces] = useState([])
-  const [errors, setErrors] = useState(false)
-  const [currentUser, setCurrentUser] = useState(false)
-
-  useEffect(() => {
-    fetch('/authorized_user')
-      .then((res) => {
-        if (res.ok) {
-          res.json()
-            .then((user) => {
-              updateUser(user);
-              fetchPlaces()
-            });
-        }
-      })
-  }, [])
-
-  const fetchPlaces = () => {
-    fetch('/places')
-      .then((res) => {
-        if (res.ok) {
-          res.json().then(setPlaces)
-        } else {
-          res.json().then(data => setErrors(data.error))
-        }
-      })
-  }
-
-  const updateUser = (user) => setCurrentUser(user)
-
-  if (errors) return <h1>{errors}</h1>
+  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <LogIn updateUser={updateUser} />
-      <div>{currentUser}</div>
-      <div>{places}</div>
-    </>
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
   )
 }
 
