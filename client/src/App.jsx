7ddfae14342094c6133743ab29a6959d0325e6d9
WizardofHause <jonrhause@gmail.com>
2022-12-06 11:41:32 -0700

    created app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
+import LogIn from './components/LogIn'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [places, setPlaces] = useState([])
+  const [errors, setErrors] = useState(false)
+  const [currentUser, setCurrentUser] = useState(false)
+
+  useEffect(() => {
+    fetch('/authorized_user')
+      .then((res) => {
+        if (res.ok) {
+          res.json()
+            .then((user) => {
+              updateUser(user);
+              fetchPlaces()
+            });
+        }
+      })
+  }, [])
+
+  const fetchPlaces = () => {
+    fetch('/places')
+      .then((res) => {
+        if (res.ok) {
+          res.json().then(setPlaces)
+        } else {
+          res.json().then(data => setErrors(data.error))
+        }
+      })
+  }
+
+  const updateUser = (user) => setCurrentUser(user)
+
+  if (errors) return <h1>{errors}</h1>
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <LogIn updateUser={updateUser} />
+      <div>{currentUser}</div>
+      <div>{places}</div>
+    </>
   )
 }
 
