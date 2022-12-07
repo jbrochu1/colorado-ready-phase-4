ac146b45d8d293ea54ee6beac7db7e326e8536b1
WizardofHause <jonrhause@gmail.com>
2022-12-07 09:57:47 -0700

    commit before pulling from main

--- app/serializers/content_serializer.rb
+++ app/serializers/content_serializer.rb
@@ -1,4 +1,5 @@
 class ContentSerializer < ActiveModel::Serializer
   attributes :place_id, :user_id, :comment, :rating
-  has_many :places
+  has_one :place
+  has_one :user
 end
--- client/src/components/PlaceCard.jsx
+++ client/src/components/PlaceCard.jsx
@@ -10,18 +10,23 @@ function PlaceCard({ place }) {
             <p>{hours}</p>
             <p>{elevation}</p>
             {kid_friendly ? <p>Kids ok!</p> : <p>Adults only</p>}
+            <ul>
             {place.contents.map(content => {
                 return(
-                    <ul>
-                        <li>
-                            <p>{content.comment}</p>
-                            <p>{content.rating}</p>
+                        <li key={content.id}>
+                            <p>"{content.comment}" - {content.user_id}</p>
+                            <p>{content.rating} / 5 STARS</p>
                         </li>
-                    </ul>
                 )
             })}
+            </ul>
         </div>
     )
 }
 
 export default PlaceCard
+// c1 = Content.first
+// c1.user.username
+//=> "miranda"
+
+//Make Content Cards and Content Container its own thing within Place Cards
