47e014f1d21949fdc7930e6178a5bbe47c95b7ab
WizardofHause <jonrhause@gmail.com>
2022-12-08 14:09:01 -0700

    modified serializers to return all related json data, regarless of the fetch request

--- client/src/components/ContentCard.jsx
+++ client/src/components/ContentCard.jsx
@@ -1,5 +1,5 @@
 function ContentCard({ content, onDeleteContent, onEditContent, place }) {
-    const { id, comment, rating, user_id } = content
+    const { id, comment, rating, user } = content
 
     
 
@@ -12,8 +12,8 @@ function ContentCard({ content, onDeleteContent, onEditContent, place }) {
 
     return (
         <div>
-            <img src={"https://pbs.twimg.com/profile_images/1237550450/mstom_400x400.jpg"} width="250" alt="https://pbs.twimg.com/profile_images/1237550450/mstom_400x400.jpg" />
-            <p>"{comment}"</p>
+            <img src={user.avatar_img} width="250" alt="https://pbs.twimg.com/profile_images/1237550450/mstom_400x400.jpg" />
+            <p>"{comment}" - {user.username}, {user.state}</p>
             <p>{rating} / 5</p>
             <button onClick={handleDelete}>DELETE</button>
         </div>
