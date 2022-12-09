0e6d02c62b41b3a70fa0aff6d909e4bc00652447
jbrochu1 <111677770+jbrochu1@users.noreply.github.com>
2022-12-09 12:35:12 -0700

    added title to home screen

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,10 @@ import PlaceContainer from './PlaceContainer'
 
 function Home({ places, updateUser, currentUser }) {
     return (
+        <div>
+        <div className='text-6xl p-5'>Colorado Ready!</div>
         <PlaceContainer places={places} updateUser={updateUser} currentUser={currentUser} />
+        </div>
     )
 }
 
