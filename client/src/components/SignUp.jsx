552455ac24503c7f47071361399cf5e73e91f41e
jbrochu1 <111677770+jbrochu1@users.noreply.github.com>
2022-12-09 10:47:23 -0700

    form styling

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -67,51 +67,53 @@ function SignUp({ updateUser }) {
                 <label>
                     Username
                 </label>
-                <input type='text' name='username' value={username} onChange={handleChange} />
+                <input type='text' name='username' className='w-2/3 float-right' value={username} onChange={handleChange} />
                 </div>
                 <div className='p-2 space-x-2'>
                 <label>
                     Email
                 </label>
-                <input type='text' name='email' value={email} onChange={handleChange} />
+                <input type='text' name='email' className='w-2/3 float-right' value={email} onChange={handleChange} />
                 </div>
                 <div className='p-2 space-x-2'>
                 <label>
                     Password
                 </label>
-                <input type='password' name='password' placeholder="Password..." value={password} onChange={handleChange} />
+                <input type='password' name='password' className='w-2/3 float-right' placeholder="Password..." value={password} onChange={handleChange} />
                 </div>
                 <div className='p-2 space-x-2'>
                 <label>
                     First Name
                 </label>
-                <input type='text' name='first_name' value={first_name} onChange={handleChange} />
+                <input type='text' name='first_name' className='w-2/3 float-right' value={first_name} onChange={handleChange} />
                 </div>
                 <div className='p-2 space-x-2'>
                 <label>
                     Last Name
                 </label>
-                <input type='text' name='last_name' value={last_name} onChange={handleChange} />
+                <input type='text' name='last_name' className='w-2/3 float-right' value={last_name} onChange={handleChange} />
                 </div>
                 <div className='p-2 space-x-2'>
                 <label>
                     Location (State)
                 </label>
-                <input type='text' name='location' value={location} onChange={handleChange} />
+                <input type='text' name='location'  className='w-1/2 float-right' value={location} onChange={handleChange} />
                 </div>
                 <div className='p-2 space-x-2'>
                 <label>
                     Age
                 </label>
-                <input type='text' name='age' value={age} onChange={handleChange} />
+                <input type='text' name='age' className='w-2/3 float-right' value={age} onChange={handleChange} />
                 </div>
                 <div className='p-2 space-x-2'>
                 <label>
                     Avatar Image
                 </label>
-                <input type='text' name='avatar_img' value={avatar_img} onChange={handleChange} />
                 </div>
-                <div className='p-2 space-x-2'>
+                <div>
+                <input type='text' name='avatar_img'  className='w-full float-right' value={avatar_img} onChange={handleChange} />
+                </div>
+                <div className='p-5'>
                 <input type='submit' value='Sign Up' className='p-3 shadow bg-indigo-600 hover:bg-indigo-500 focus:shadow-outline focus:outline-none text-white font-bold rounded'/>
                 </div>
             </form>
