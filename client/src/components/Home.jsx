b8a3b704c61575ecd48fe0613151763fd7afed24
jbrochu1 <111677770+jbrochu1@users.noreply.github.com>
2022-12-09 12:38:39 -0700

    added title gradient

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,7 +3,7 @@ import PlaceContainer from './PlaceContainer'
 function Home({ places, updateUser, currentUser }) {
     return (
         <div>
-        <div className='text-6xl p-5'>Colorado Ready!</div>
+        <div className='text-6xl p-5 font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Colorado Ready!</div>
         <PlaceContainer places={places} updateUser={updateUser} currentUser={currentUser} />
         </div>
     )
