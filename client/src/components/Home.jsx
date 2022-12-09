1032f8e6b7cd5450a1facf2f303bb5f30cdd8bd5
jbrochu1 <111677770+jbrochu1@users.noreply.github.com>
2022-12-09 13:39:55 -0700

    added colorado logo

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,8 +3,15 @@ import PlaceContainer from './PlaceContainer'
 function Home({ places, updateUser, currentUser }) {
     return (
         <div>
-        <div className='text-6xl p-5 font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Colorado Ready!</div>
-        <PlaceContainer places={places} updateUser={updateUser} currentUser={currentUser} />
+            <div className="flex items-center">
+                <div>
+                    <img className='max-w-xs' src="https://www.pngmart.com/files/22/Colorado-Flag-PNG-Photos.png" /> 
+                </div>
+                    <div className='p-5 font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>olorado Ready!</div>
+            </div>        
+            <div>
+                <PlaceContainer places={places} updateUser={updateUser} currentUser={currentUser} />
+            </div>
         </div>
     )
 }
